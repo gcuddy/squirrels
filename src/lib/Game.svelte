59d62b8a7e0d2d5051200a106e50aa1e15d104f6
Gus <gus.cuddy@gmail.com>
2023-11-07 19:10:50 -0500

    basics working

---
+++ src/lib/Game.svelte
@@ -0,0 +1,81 @@
+<script>
+  import { onMount } from "svelte";
+
+  /**
+   * @type {{humans: string[]; robots: string[]}}
+   */
+  export let data;
+
+  function getStories() {
+    const humanIdx = Math.floor(Math.random() * data.humans.length);
+    const humanStory = data.humans[humanIdx];
+
+    const robotIdx = Math.floor(Math.random() * data.robots.length);
+    const robotStory = data.robots[robotIdx];
+
+    const robotLeft = Math.random() > 0.5;
+
+    if (robotLeft) {
+      return [
+        {
+          story: robotStory,
+          type: "robot",
+        },
+        {
+          story: humanStory,
+          type: "human",
+        },
+      ];
+    } else {
+      return [
+        {
+          story: humanStory,
+          type: "human",
+        },
+        {
+          story: robotStory,
+          type: "robot",
+        },
+      ];
+    }
+  }
+
+  let stories = getStories();
+
+  //   onMount(() => {
+  //     stories = getStories();
+  //   })
+</script>
+
+<div class="container">
+  <div
+    on:click={() => {
+      if (stories[0].type === "robot") {
+        alert("You got it right!");
+      } else {
+        alert("You got it wrong!");
+      }
+    }}
+  >
+    {stories[0].story}
+  </div>
+  <div
+    on:click={() => {
+      if (stories[1].type === "robot") {
+        alert("You got it right!");
+      } else {
+        alert("You got it wrong!");
+      }
+    }}
+  >
+    {stories[1].story}
+  </div>
+</div>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+  let title = "World";
+  import Game from "../lib/Game.svelte";
+  import Squirrels from "../lib/Squirrels.svelte";
+  import Wordcloud from "../lib/wordcloud.svelte";
+  $: secondTitle = title + " Squirrels";
+
+  import stories from '$lib/stories.json';
+</script>
+
+<div class="header">
+  <h1>Squirrel Stories</h1>
+  <div>Real or fake?</div>
+</div>
+<div class="wordcloud">
+  <Wordcloud />
+</div>
+
+<Game
+  data={stories}
+/>
+
+<style>
+  .header {
+    margin-bottom: 20px;
+  }
+
+  .wordcloud {
+    transform: scale(0.5);
+    margin: -148px;
+  }
+</style>
